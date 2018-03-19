287a894f783edc2d87af2692b412dece2d17d301
ConstanceEDJ <constance.daire@sciencespo.fr>
2018-03-19 19:46:48 +0100

    Switch to youtube client

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-$(document).ready(function() {
+var players;
+var mainScene;
+
+function onYouTubeIframeAPIReady() {
   // init controller
   var controller = new ScrollMagic.Controller();
 
@@ -17,43 +20,98 @@ $(document).ready(function() {
 
 
   // create a scene
-  new ScrollMagic.Scene({
+  mainScene = new ScrollMagic.Scene({
       triggerElement: '#container',
       triggerHook: 'onLeave',
-      duration: '1100%' // = nombre de slides * 100 (pour un scroll naturel)
+      duration: '800%' // = nombre de slides * 100 (pour un scroll naturel)
     })
     .setPin('#container')
     .setTween(wipeAnimation)
-    .on("update", function (scene) {
-      const progress = scene.target.progress()
-       if (progress < (1/11)) {
-        startVideo('intro')
-      } else {
-        stopVideo('intro')
-      }
-      if (progress > (2/11) && progress < (4/11)) {
-        startVideo('chapitre-2')
-      } else {
-        stopVideo('chapitre-2')
-      }
-      if (progress > (5.5/11) && progress < (7/11)) {
-        startVideo('chapitre-2c')
-      } else {
-        stopVideo('chapitre-2c')
-      }
-    })
+    .on("update", scrollScene)
     .addIndicators()
     .addTo(controller);
-});
 
-function startVideo(chapitre) {
-  $('#'+chapitre+' video').addClass('active')
-  $('#'+chapitre+' video')[0].play()
+  players = {
+    'intro': initPlayer('intro-player'),
+    'chapitre-2': initPlayer('chapitre-2-player'),
+  }
 }
 
+function startVideo(chapitre) {
+  // $('#'+chapitre+' video').addClass('active')
+  // $('#'+chapitre+' video')[0].play()
+  if (players[chapitre] && players[chapitre].playVideo) {    
+    players[chapitre].playVideo()
+  }
+}
+
+
 function stopVideo(chapitre) {
-  $('#'+chapitre+' video').removeClass('active')
-  $('#'+chapitre+' video')[0].pause()
+  if (players[chapitre] && players[chapitre].stopVideo) {    
+    players[chapitre].stopVideo()
+  }
+}
+
+function initPlayer(id) {
+  var element = $("#" + id)
+  return new YT.Player(id, {
+    height: '100%',
+    width: '100%',
+    videoId: element.data('video-id'),
+    playerVars: {
+      autoplay: 0, 
+      controls: 0,
+      loop: 1,
+      playlist: element.data('video-id'),
+      showinfo: 0,
+      modestbranding: 1,
+      start: 70
+    },
+    events: {
+      'onReady': function() {
+        scrollScene()
+        resizeVideo(id)
+      }
+    }
+  });
+}
+
+function scrollScene() {
+  const progress = mainScene.progress()
+  if (progress < (1/11)) {
+    startVideo('intro')
+  } else {
+    stopVideo('intro')
+  }
+  if (progress > (2/11) && progress < (4/11)) {
+    startVideo('chapitre-2')
+  } else {
+    stopVideo('chapitre-2')
+  }
+  if (progress > (5.5/11) && progress < (7/11)) {
+    startVideo('chapitre-2c')
+  } else {
+    stopVideo('chapitre-2c')
+  }
+}
+
+function resizeVideo(id) {  
+  var video = $('#' + id);
+
+  if(window.innerWidth > window.innerHeight) {    
+    var newWidth = video.outerHeight() * (16  / 9);
+    var scale = window.innerWidth / newWidth
+    video.css('width', newWidth + "px")
+    video.css('transform-origin', 'left center')
+  } else { 
+    var newHeight = video.outerWidth() * (9 / 16);
+    var scale = window.innerHeight / newHeight
+    video.css('height', newHeight + "px")
+    video.css('transform-origin', 'center top')
+  }
+  
+  //Define the new width and centrally align the iframe
+  video.css("transform", "scale(" + scale + ")");
 }
 
 /*
